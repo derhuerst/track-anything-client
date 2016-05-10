16ccfb2e5ac818fabfae6a96a86488d33b91fde0
Jannis R <mail@jannisr.de>
2016-05-10 03:14:51 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 'use strict'
 
-// todo
+const url  = require('url')
+const http = require('http')
+const qs   = require('querystring')
+const sink = require('stream-sink')
+const noop = () => {}
 
-module.exports = null
+
+
+const tracker = (opt = {}) => {
+	if ('string' !== typeof opt.host) throw new Error('Missing host.')
+	if ('number' !== typeof opt.port) throw new Error('Missing port.')
+	if ('string' !== typeof opt.tracker) throw new Error('Missing tracker.')
+	if ('string' !== typeof opt.key) throw new Error('Missing key.')
+	return (cb = noop) => {
+		let req = http.request({
+			  method: 'PATCH'
+			, host:   opt.host
+			, port:   opt.port
+			, path:   '/trackers/' + encodeURIComponent(opt.tracker)
+			, headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		})
+		req.on('error', cb)
+		req.on('response', (res) => {
+			if (200 <= res.statusCode && res.statusCode < 300) return cb()
+			res.pipe(sink()).on('data', (body) => {
+				try {body = JSON.parse(body)}
+				catch (e) {cb(new Error('Invalid response.'))}
+				cb(new Error(body.msg))
+			})
+		})
+		req.end(qs.stringify({key: opt.key}))
+	}
+}
+
+module.exports = tracker
